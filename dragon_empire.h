b76af2e696cbb7dc56b2d449479e1f137933cc70
Sergeiprogrammer <143258604+Sergeiprogrammer@users.noreply.github.com>
2026-02-17 19:16:08 +0100

    Add files via upload

---
+++ dragon_empire.h
@@ -0,0 +1,67 @@
+﻿#ifndef DRAGON_EMPIRE_H
+#define DRAGON_EMPIRE_H
+
+#include <stdio.h>
+
+/* ============================= */
+/* ===== Типы Империи ===== */
+/* ============================= */
+
+#define 我宣誓效忠伟大帝国的生产力 int
+#define 天地虚无之境 void
+#define 符文载体 char
+#define 千年不朽之力 long
+#define 凡尘短暂之力 short
+#define 灵气流转之术 float
+#define 双倍龙魂之气 double
+#define 命运之抉择 _Bool
+
+/* ============================= */
+/* ===== Управление Судьбой ===== */
+/* ============================= */
+
+#define 倘若天命如此安排 if
+#define 否则听从宇宙安排 else
+#define 否则若命运再次转折 else if
+#define 天命之分岔 switch
+#define 若此命运降临 case
+#define 若无其他命数 default
+#define 修行仍在继续之时 while
+#define 先行动后思考 do
+#define 循环修炼之道 for
+#define 强行穿越时空 goto
+#define 继续闭关修炼 continue
+#define 强行打断因果 break
+#define 向伟大帝国汇报成果 return
+
+/* ============================= */
+/* ===== Объявления Мудрости ===== */
+/* ============================= */
+
+#define 静默守护之术 static
+#define 来自远方的支援 extern
+#define 顺其自然之力 auto
+#define 寄存在龙脉之中 register
+#define 不可违背的誓言 const
+#define 变化无常的命运 volatile
+#define 内在融合之法 inline
+#define 测量万物之尺度 sizeof
+#define 赐予新名之仪式 typedef
+
+/* ============================= */
+/* ===== Формы Бытия ===== */
+/* ============================= */
+
+#define 组合万象之躯 struct
+#define 阴阳合一之体 union
+#define 列举众生之名 enum
+
+/* ============================= */
+/* ===== Верховная Власть ===== */
+/* ============================= */
+
+#define 伟大领袖亲自降临 main
+#define 向天下苍生昭告真理 printf
+#define 聆听众生低语 scanf
+
+#endif
