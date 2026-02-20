f4915e9969a8d9a37842a96836345fb03f48b292
Sergeiprogrammer <143258604+Sergeiprogrammer@users.noreply.github.com>
2026-02-20 17:02:36 +0100

    Create example.c

---
+++ example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dragon_warior.h"
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h> // Includes the standard input-output library
+
+int main() {
+  /* main() is where program execution begins */
+  printf("Hello, World!\n"); // Prints the string to the screen
+  return 0; // Indicates successful program termination
+}
